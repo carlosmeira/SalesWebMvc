04681c3481eec6203647e6866e2e1206563704c2
Carlos Meira <meira.bcc@gmail.com>
2023-04-05 22:13:42 -0300

    Seller Details

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -59,5 +59,61 @@ namespace SalesWebMvc.Controllers
 			_sellerService.Remove(id);
 			return RedirectToAction(nameof(Index));
 		}
+
+        public IActionResult Details(int? id)
+        {
+			if (id == null)
+            {
+				return NotFound();
+			}
+			var obj = _sellerService.FindById(id.Value);
+			if (obj == null)
+            {
+				return NotFound();
+			}
+			return View(obj);
+		}
+		public IActionResult Edit(int? id)
+        {
+			if (id == null)
+            {
+				return NotFound();
+			}
+			var obj = _sellerService.FindById(id.Value);
+			if (obj == null)
+            {
+				return NotFound();
+			}
+			List<Department> departments = _departmentService.FindAll();
+			SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+			return View(viewModel);
+		}
+		/*[HttpPost]
+		[ValidateAntiForgeryToken]
+		/*public IActionResult Edit(int id, Seller seller)
+		{
+			if (id != seller.Id)
+			{
+				return BadRequest();
+			}
+			try
+			{
+				_sellerService.Update(seller);
+				return RedirectToAction(nameof(Index));
+			}
+			catch (ApplicationException e)
+			{
+				return RedirectToAction(nameof(Error), new { message = e.Message });
+			}
+		}
+		public IActionResult Error(string message)
+		{
+			var viewModel = new ErrorViewModel
+			{
+				Message = message,
+				RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+			};
+			return View(viewModel);
+		}*/
     }
 }
